cbe2d8269e40e870d6558d73437f84243e925234
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2024-12-02 05:52:42 -0800

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,1 +1,13 @@
-pyinstaller main.py --hidden-import=yaml --hidden-import=yaml --exclude-module=pygame --exclude-module=PyQt5 --exclude-module=PySide6 --exclude-module=cipher --onefile --icon="./icon.ico" --name="cipheros_build"
+#!/bin/bash
+# Set output directory
+OUTPUT_DIR="dist/linux"
+if [[ "$(uname)" == "Darwin" ]]; then
+  OUTPUT_DIR="dist/macos"
+fi
+
+# Create the directory if it doesn't exist
+mkdir -p "$OUTPUT_DIR"
+
+# Run PyInstaller
+
+pyinstaller main.py --distpath %OUTPUT_DIR% --hidden-import=yaml --hidden-import=yaml --exclude-module=pygame --exclude-module=PyQt5 --exclude-module=PySide6 --exclude-module=cipher --onefile --icon="./icon.ico" --name="cipheros_build" 

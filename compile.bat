7eb86f887529fe1da1a12b6bdfe852c8de2729ce
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2024-12-02 05:53:04 -0800

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -7,4 +7,4 @@ if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
 
 REM Run PyInstaller
 
-pyinstaller main.py --hidden-import=yaml --hidden-import=yaml --exclude-module=pygame --exclude-module=PyQt5 --exclude-module=PySide6 --onefile --icon="./icon.ico" --name="cipheros_test"
+pyinstaller main.py --distpath %OUTPUT_DIR% --hidden-import=yaml --hidden-import=yaml --exclude-module=pygame --exclude-module=PyQt5 --exclude-module=PySide6 --onefile --icon="./icon.ico" --name="cipheros_test"

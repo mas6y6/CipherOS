e985e93737f92978474126f1a4761e79f61cf2f5
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2024-12-02 05:51:00 -0800

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,1 +1,10 @@
+@echo off
+REM Set output directory
+set OUTPUT_DIR=dist\windows
+
+REM Create the directory if it doesn't exist
+if not exist %OUTPUT_DIR% mkdir %OUTPUT_DIR%
+
+REM Run PyInstaller
+
 pyinstaller main.py --hidden-import=yaml --hidden-import=yaml --exclude-module=pygame --exclude-module=PyQt5 --exclude-module=PySide6 --onefile --icon="./icon.ico" --name="cipheros_test"
